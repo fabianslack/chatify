e222a100ee3860eceffa97bed5a485857fce7ce1
jannik2903 <jannik.hannemann@gmail.com>
2020-08-18 18:05:00 +0200

    update

--- lib/pages/Root_page.dart
+++ lib/pages/Root_page.dart
@@ -2,16 +2,14 @@ import 'package:chatapp/themes/theme.dart';
 import 'package:flutter/material.dart';
 import 'home/home.dart';
 import 'profile_page.dart';
-import 'story_page.dart';
+import 'usersettings_page.dart';
 
-class RootPage extends StatefulWidget
-{
+class RootPage extends StatefulWidget {
   @override
   _RootPageState createState() => _RootPageState();
 }
 
-class _RootPageState extends State<RootPage> 
-{
+class _RootPageState extends State<RootPage> {
   final PageController _controller = PageController();
   int _selectedIndex = 0;
   Home _home;
@@ -20,25 +18,17 @@ class _RootPageState extends State<RootPage>
   List<Widget> _bodies;
 
   @override
-  void initState()
-  {
+  void initState() {
     _home = Home();
     _profilePage = ProfilePage();
     _storyPage = ProfileSettingsPage();
-    _bodies = [
-      _home, 
-      _profilePage,
-      _storyPage
-    ];
-
+    _bodies = [_home, _profilePage, _storyPage];
   }
 
-  Widget getBottomNavigationBar()
-  {
+  Widget getBottomNavigationBar() {
     return BottomNavigationBar(
       elevation: 0,
-      onTap: (index) 
-      {
+      onTap: (index) {
         _controller.jumpToPage(index);
       },
       currentIndex: _selectedIndex,
@@ -51,42 +41,44 @@ class _RootPageState extends State<RootPage>
       items: [Icons.chat, Icons.share, Icons.supervised_user_circle]
           .asMap()
           .map((key, value) => MapEntry(
-          key, BottomNavigationBarItem(
-          title: Text(''),
-          icon: Container(
-            padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 16.0),
-            decoration: BoxDecoration(
-                color: _selectedIndex == key
-                    ? Colors.red
-                    : Colors.transparent,
-                borderRadius: BorderRadius.circular(20.0)
-            ),
-            child: Icon(value, color: Colors.white,),
-          )
-      )
-      )
-      ).values.toList(),
+              key,
+              BottomNavigationBarItem(
+                  title: Text(''),
+                  icon: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 6.0, horizontal: 16.0),
+                    decoration: BoxDecoration(
+                        color: _selectedIndex == key
+                            ? Colors.red
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Icon(
+                      value,
+                      color: Colors.white,
+                    ),
+                  ))))
+          .values
+          .toList(),
     );
   }
 
-  Widget getBody() 
-  {
-   return PageView(
-     children: _bodies,
-     onPageChanged: (value)
-     {
-       setState(() 
-       {
-          _selectedIndex = value;  
-       });
-     },
-     controller: _controller,
-   ); 
+  Widget getBody() {
+    return PageView(
+      children: _bodies,
+      onPageChanged: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+      },
+      controller: _controller,
+    );
   }
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold(body: getBody(), bottomNavigationBar: getBottomNavigationBar(),);
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getBody(),
+      bottomNavigationBar: getBottomNavigationBar(),
+    );
   }
 }
